41657acbbdb7f506ecf795ed392dc23f6baab3e5
yupiliano740-sys <yupiliano740@gmail.com>
2026-03-14 21:21:26 -0400

    Change SCRIPT_URL to GitHub repository link
    Updated SCRIPT_URL to point to a new GitHub location.

--- loader.lua
+++ loader.lua
@@ -2,7 +2,7 @@
 -- By: TuLoader
 
 -- URL del script en Pastefy
-local SCRIPT_URL = "https://pastefy.app/tDPnbSVP/raw"
+local SCRIPT_URL = "https://raw.githubusercontent.com/yupiliano740-sys/COPYJOUSE/refs/heads/main/Copyjouse"
 
 -- Función para cargar y ejecutar el script remoto
 local function loadRemoteScript(url)
